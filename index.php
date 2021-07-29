4b835fd409ee36a675baa7e6cd2940cd2b3d85e6
Tofazzal Hossain <tofa.du@gmail.com>
2021-07-30 02:29:20 +0600

    Home Page updated

--- index.php
+++ index.php
@@ -12,48 +12,102 @@ if (isset($_GET['logout'])) {
     header("location: login.php");
 }
 ?>
-
-
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <!-- Bootstrap CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <title>Registration system PHP and MySQL</title>
     <!-- Custom styles for this template -->
-</head>
-<body>
-<div class="container-sm">
-    <div class="row justify-content-center">
-        <div class="card m-5 p-0" style="width: 80rem;">
-            <div class="card-header  text-white bg-primary text-center fs-2 fw-bold">
-                Home Page
-            </div>
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
 
-            <div class="card-body mx-3">
-                <!-- notification message -->
-                <?php if (isset($_SESSION['success'])) : ?>
-                    <div class="error success">
-                        <h3>
-                            <?php
-                            echo $_SESSION['success'];
-                            unset($_SESSION['success']);
-                            ?>
-                        </h3>
-                    </div>
-                <?php endif ?>
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/blog.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container">
+
+    <header class="blog-header py-3">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+            <div class="col-4">
+            </div>
+            <div class="col-4 text-center">
+                <a class="blog-header-logo text-dark" href="#">Blog Home Page</a>
+            </div>
+            <div class="col-4 d-flex justify-content-end align-items-center">
 
                 <!-- logged in user information -->
                 <?php if (isset($_SESSION['email'])) : ?>
-                    <h5 class="card-title">Welcome <span class="text-danger"><?php echo $_SESSION['username']; ?></span></h5>
+                    <h5 class="text-primary"><?php echo $_SESSION['username']; ?></h5>
                     <a class="btn btn-primary" role="button" href="index.php?logout='1'">Logout</a>
+                <?php elseif (!isset($_SESSION['email'])) : ?>
+                    <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
                 <?php endif ?>
             </div>
         </div>
+    </header>
+
+    <div class="nav-scroller py-1 mb-2">
+        <nav class="nav d-flex justify-content-between">
+            <a class="p-2 link-secondary" href="#">World</a>
+            <a class="p-2 link-secondary" href="#">U.S.</a>
+            <a class="p-2 link-secondary" href="#">Technology</a>
+            <a class="p-2 link-secondary" href="#">Design</a>
+            <a class="p-2 link-secondary" href="#">Culture</a>
+            <a class="p-2 link-secondary" href="#">Business</a>
+            <a class="p-2 link-secondary" href="#">Politics</a>
+            <a class="p-2 link-secondary" href="#">Opinion</a>
+            <a class="p-2 link-secondary" href="#">Science</a>
+            <a class="p-2 link-secondary" href="#">Health</a>
+            <a class="p-2 link-secondary" href="#">Style</a>
+            <a class="p-2 link-secondary" href="#">Travel</a>
+        </nav>
     </div>
+</div>
+
+<div class="row justify-content-center">
+    <div class="card m-5 p-0" style="width: 80rem;">
+
+
+        <div class="card-body mx-3">
+            <!-- notification message -->
+            <?php if (isset($_SESSION['success'])) : ?>
+                <div class="error success">
+                    <h3>
+                        <?php
+                        echo $_SESSION['success'];
+                        unset($_SESSION['success']);
+                        ?>
+                    </h3>
+                </div>
+            <?php endif ?>
+
+
+        </div>
+    </div>
+</div>
 </div>
 
 <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
+
 </html>
